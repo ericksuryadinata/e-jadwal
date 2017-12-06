05d9d3b64fd41c842575044df57c8fb536607fd4
proesd <ericksuryadinata@gmail.com>
2017-12-06 20:45:00 +0700

    title

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <html>
     <head>
-
+        <title>Fakultas Teknik | Universitas 17 Agustus 1945 Surabaya </title>
     </head>
     <body>
         
